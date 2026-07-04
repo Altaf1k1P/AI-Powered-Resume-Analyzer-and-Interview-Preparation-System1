46a1eefcb04735734ffad2cfff3cb1351821a862
Altaf Khan <altafpathan1k1@gmail.com>
2026-07-04 23:36:45 +0530

    feat(backend): implement user login controller and routing with bcrypt verification and JWT generation

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require(mongoose);
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5005;
+const MONGO_URI = process.env.MONGO_URI  
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(MONGO_URI)
+.then(
+  ()=>{
+    console.log('Successfully connected to MongoDB')
+  }
+)
+.catch(
+  (err)=>{
+    console.error('MongoDB connection error:',err)
+  }
+)
 
 // Basic test route
 app.get('/', (req, res) => {
